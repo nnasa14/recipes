7c52e7db4bd7ab4c8d46b425275198d3a33aa0fc
nnasa14 <nnascimento.kr@gmail.com>
2023-07-27 11:13:45 -0400

    adding enhancements to remote_storage.py

--- remote_storage.py
+++ remote_storage.py
@@ -1,12 +1,12 @@
 import recipes
 import ingredients
-import __init__
 import sqlalchemy
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import sessionmaker
+
 Base = declarative_base()
-engine = create_engine('db_url')
+engine = create_engine('sqlite://my_database.db')
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -14,10 +14,14 @@ session = Session()
 class Recipes(Base):
     __tablename__ = "recipes"
     id = Column(Integer, primary_key = True)
-    title = Column(String)
-    ingredients = Column(String)
+    title = Column(String, unique=True, nullable=False)
+    ingredients = Column(String, nullable=False)
 
 if __name__ == "__main__":
     new_recipe = Recipes(title = "banana bread", ingredients = "banana")
     session.add(new_recipe)
     session.commit()
+
+    #recipes = session.query(Recipes).filter(Recipes.title = "banana bread").all()      #search recipe
+
+    #refer to the namespace
